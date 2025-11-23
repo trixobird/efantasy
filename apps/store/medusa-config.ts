817358bc4313adab64c12377863375b8a5d292ee
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2025-11-23 08:31:08 +0200

    add backendUrl

--- apps/store/medusa-config.ts
+++ apps/store/medusa-config.ts
@@ -3,6 +3,9 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
